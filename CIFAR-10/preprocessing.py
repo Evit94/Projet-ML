93b586feb5a956dbc9d2be70a868557739b913c4
Eric <eric.eung.2005@gmail.com>
2026-04-29 14:15:28 +0200

    Ajout de la partie 2, non finalisé

---
+++ CIFAR-10/preprocessing.py
@@ -0,0 +1,33 @@
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import keras
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+
+(X_train, Y_train), (X_test, Y_test) = keras.datasets.cifar10.load_data()
+
+R_train = X_train[:,:,:,0]
+G_train = X_train[:,:,:,1]
+B_train = X_train[:,:,:,2]
+X_gris_train = 0.299*R_train + 0.587*G_train + 0.114*B_train
+X_gris_train = X_gris_train.reshape(50000, 1024)
+
+R_test = X_test[:,:,:,0]
+G_test = X_test[:,:,:,1]
+B_test = X_test[:,:,:,2]
+X_gris_test = 0.299*R_test + 0.587*G_test + 0.114*B_test
+X_gris_test = X_gris_test.reshape(10000,1024)
+
+#Normalisation 
+X_gris_train = X_gris_train/255.0
+X_gris_test = X_gris_test/255.0
+
+#One-hot encoding
+Y_train = to_categorical(Y_train.flatten())
+Y_test = to_categorical(Y_test.flatten())
+
+print(X_gris_train.shape)   # (50000, 1024)
+print(X_gris_test.shape)    # (10000, 1024)
+print(X_gris_train.max())   # 1.0
+print(Y_train.shape)        # (50000, 10)
